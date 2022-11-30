85ad2134c4ccbf98cb15b59d03e593df7f40d82d
vatchapol jittipattanakulchai <jitti.geng@gmail.com>
2022-11-30 14:39:53 +0700

    second commit

--- src/main/java/com/eaztbytes/loans/controller/LoansController.java
+++ src/main/java/com/eaztbytes/loans/controller/LoansController.java
@@ -7,15 +7,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.eaztbytes.loans.config.LoansServiceConfig;
 import com.eaztbytes.loans.model.Customer;
 import com.eaztbytes.loans.model.Loans;
+import com.eaztbytes.loans.model.Properties;
 import com.eaztbytes.loans.repository.LoansRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 @RestController
 public class LoansController {
 
 	@Autowired
 	private LoansRepository loansRepository;
+	
+	@Autowired
+	LoansServiceConfig loansConfig;
 
 	@GetMapping("/myLoans")
 	public List<Loans> getLoansDetails(@RequestBody Customer customer) {
@@ -26,6 +34,15 @@ public class LoansController {
 			return null;
 		}
 
+	}
+	
+	@GetMapping("/loans/properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(loansConfig.getMsg(), loansConfig.getBuildVersion(),
+				loansConfig.getMailDetails(), loansConfig.getActiveBranches());
+		String jsonStr = ow.writeValueAsString(properties);
+		return jsonStr;
 	}
 
 }
